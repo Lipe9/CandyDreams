8edde8ca0d1bb8fa6d233850e24b1e0601928011
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-04-18 13:05:28 -0300

    Atualizar o adm.js

--- js/adm.js
+++ js/adm.js
@@ -27,3 +27,30 @@ function saveProduct(product) {
   alert('Produto adicionado com sucesso!');
   document.getElementById('product-form').reset();
 }
+
+
+
+// código do carrinho.js
+
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.produtos-container');
+    container.innerHTML = ''; // limpa os produtos fixos
+
+    const produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+
+    produtos.forEach(p => {
+      const card = document.createElement('div');
+      card.className = 'produto-card';
+      card.innerHTML = `
+        <img src="${p.imagem}" alt="${p.nome}" />
+        <h3>${p.nome}</h3>
+        <p>${p.descricao}</p>
+        <p class="preco">R$ ${p.valor.toFixed(2)}</p>
+        <p><strong>Variedades:</strong> ${p.variedades}</p>
+        <p><strong>Tamanho:</strong> ${p.tamanho}</p>
+        <p><strong>Peso:</strong> ${p.peso}</p>
+        <button class="botao-comprar" onclick="adicionarAoCarrinho('${p.nome}', ${p.valor}, '${p.imagem}')">Comprar</button>
+      `;
+      container.appendChild(card);
+    });
+  });
