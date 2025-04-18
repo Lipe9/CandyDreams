f5e8936591544b4f40a0da52886518f545383c74
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-04-18 12:46:14 -0300

    Criar adm.js

---
+++ js/adm.js
@@ -0,0 +1,29 @@
+document.getElementById('product-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const product = {
+    name: document.getElementById('name').value,
+    description: document.getElementById('description').value,
+    varieties: document.getElementById('varieties').value.split(','),
+    size: document.getElementById('size').value,
+    weight: document.getElementById('weight').value,
+    price: parseFloat(document.getElementById('price').value),
+    image: '' // será preenchido após o upload
+  };
+
+  const imageInput = document.getElementById('image');
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    product.image = event.target.result;
+    saveProduct(product);
+  };
+  reader.readAsDataURL(imageInput.files[0]);
+});
+
+function saveProduct(product) {
+  let products = JSON.parse(localStorage.getItem('products')) || [];
+  products.push(product);
+  localStorage.setItem('products', JSON.stringify(products));
+  alert('Produto adicionado com sucesso!');
+  document.getElementById('product-form').reset();
+}
